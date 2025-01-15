d5a7d80ae3f80791b435b133ed02f2c265b1fbe5
sarvesh <sarvesh.w@somaiya.edu>
2025-01-16 00:23:43 +0530

    MF.js Changed

--- src/pages/MutualFundPage.js
+++ src/pages/MutualFundPage.js
@@ -82,6 +82,10 @@ const MutualFundPage = () => {
   </div>
 </section>
 
+<section className="dark:bg-gray-900 text-[#444444] dark:text-gray-300 font-roboto transition-colors duration-300">
+    <SIPCalculator />
+  </section>
+
 {/* Investment Options Section */}
 <section className="bg-blue-50 dark:bg-gray-800 py-12 px-6 md:px-20 mt-0 dark:bg-gray-900 text-[#444444] dark:text-gray-300 font-roboto transition-colors duration-300">
   <h2 className="text-3xl font-extrabold text-center text-blue-600 dark:text-blue-300 mb-8">Mutual Funds to Invest In</h2>
