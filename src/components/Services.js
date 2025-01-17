eead9301d0169efecf543ac021d66938caa35ce6
sarvesh <sarvesh.w@somaiya.edu>
2025-01-17 12:37:23 +0530

    removed space

--- src/components/Services.js
+++ src/components/Services.js
@@ -9,10 +9,10 @@ const Services = () => {
   id="services"
   className="w-full min-h-screen px-4 py-16 bg-white dark:bg-gray-900 text-[#444444] dark:text-gray-300 font-roboto transition-colors duration-300"
   >
-    
+
   <div className="w-full max-w-screen-xl mx-auto px-6 py-12">
     {/* Section Title */}
-    <h2 className="text-3xl md:text-4xl font-bold text-[#384f4b] text-center mb-4 font-inter dark:text-white">
+    <h2 className="text-3xl md:text-4xl font-bold text-[#384f4b] text-center  font-inter dark:text-white mb-4">
       Our Services
     </h2>
     <div className="w-24 h-1 bg-blue-600 mx-auto mb-8 dark:bg-blue-400"></div>
