ebe69b267b52c5412f294101c8840f6fe4dd1056
sarvesh <sarvesh.w@somaiya.edu>
2025-02-11 11:12:57 +0530

    footer icons changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import { FaFacebookF, FaInstagram, FaLinkedin, FaYoutube,  } from 'react-icons/fa';
-import {FaFacebookF, FaInstagram, FaTwitter, FaLinkedinIn, FaYoutube, FaTelegramPlane,FaEnvelope, FaPhone, FaMapMarkerAlt, FaTwitterSquare } from "react-icons/fa";
+import {FaFacebookF, FaInstagram, FaTwitter, FaLinkedinIn, FaYoutube, FaTelegramPlane,FaEnvelope, FaPhone, FaMapMarkerAlt, FaTwitterSquare, FaXTwitter } from "react-icons/fa";
 
 // import { FaSquareXTwitter } from 'react-icons/fa6';
 
@@ -164,7 +164,7 @@ const Footer = () => {
 
       <a className="hover:opacity-75" href="#" target="_blank" rel="noreferrer">
         <span className="sr-only">Twitter</span>
-        <FaTwitterSquare className="w-6 h-6" />
+        <FaXTwitter className="w-6 h-6" />
       </a>
 
       <a className="hover:opacity-75" href="#" target="_blank" rel="noreferrer">
