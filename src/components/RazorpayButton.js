dcaa1dfecbc689b005f846118a5b6dcf3acc8e49
sarvesh <sarvesh.w@somaiya.edu>
2025-04-25 16:07:04 +0530

    RazorPay button testing

--- src/components/RazorpayButton.js
+++ src/components/RazorpayButton.js
@@ -4,7 +4,7 @@ const RazorpayButton = () => {
   useEffect(() => {
     const script = document.createElement('script');
     script.src = 'https://checkout.razorpay.com/v1/payment-button.js';
-    script.setAttribute('data-payment_button_id', 'pl_QNFhTmuA90zsIi');
+    script.setAttribute('data-payment_button_id', 'pl_QNG8nUGgPOEyzj');
     script.async = true;
 
     const form = document.getElementById('razorpay-form');
