e6ff545cd6bdecece4f2adee6dc8871ebe082a93
sarvesh <sarvesh.w@somaiya.edu>
2025-06-18 12:39:08 +0530

    updated ticker

--- src/components/FloatingTicker.js
+++ src/components/FloatingTicker.js
@@ -7,27 +7,27 @@ const FloatingTicker = () => {
     script.async = true;
     script.innerHTML = JSON.stringify({
       symbols: [
-        {
-          proName: "FOREXCOM:SPXUSD",
-          title: "S&P 500 Index"
-        },
-        {
-          proName: "FOREXCOM:NSXUSD",
-          title: "US 100 Cash CFD"
-        },
-        {
-          proName: "FX_IDC:EURUSD",
-          title: "EUR to USD"
-        },
-        {
-          proName: "BITSTAMP:BTCUSD",
-          title: "Bitcoin"
-        },
-        {
-          proName: "BITSTAMP:ETHUSD",
-          title: "Ethereum"
-        }
-      ],
+
+            { proName: "BSE:SENSEX", title: "SENSEX" },
+            { proName: "NSE:NIFTY", title: "NIFTY" },
+            { proName: "NSE:BANKNIFTY", title: "BANKNIFTY" },
+            { proName: "NSE:CNXFINANCE", title: "CNXFINANCE" },
+            { proName: "NSE:NIFTY_MID_SELECT", title: "NIFTY MID SELECT" },
+            { proName: "NSE:ABB", title: "ABB" },
+            { proName: "NSE:COFORGE", title: "COFORGE" },
+            { proName: "NSE:DIVISLAB", title: "DIVISLAB" },
+            { proName: "NSE:DIXON", title: "DIXON" },
+            { proName: "NSE:HEROMOTOCO", title: "HEROMOTOCO" },
+            { proName: "NSE:MARUTI", title: "MARUTI" },
+            { proName: "NSE:MCX", title: "MCX" },
+            { proName: "NSE:OFSS", title: "OFSS" },
+            { proName: "NSE:SIEMENS", title: "SIEMENS" },
+            { proName: "NSE:TRENT", title: "TRENT" },
+            { proName: "NSE:ULTRACEMCO", title: "ULTRATECH" },
+            { proName: "NSE:NAUKRI", title: "NAUKRI" }
+
+          ],
+
       showSymbolLogo: true,
       isTransparent: false,
       displayMode: "adaptive",
@@ -44,8 +44,6 @@ const FloatingTicker = () => {
   return (
     <div
       id="tradingview-widget-container"
-      // className="fixed bottom-4 right-4 w-[350px] h-[60px] z-50 rounded-md overflow-hidden shadow-lg"
-      // className="fixed bottom-0 left-0 w-full h-[40px] z-50 shadow-md bg-white dark:bg-black"
        className="fixed top-0 left-0 w-full h-[40px] z-50 shadow-md bg-white dark:bg-black"
     >
       {/* TradingView widget script will be injected here */}
