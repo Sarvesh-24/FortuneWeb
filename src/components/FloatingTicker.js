ca49380b94a2c60baa8e0576a1d5bbd736dec33f
sarvesh <sarvesh.w@somaiya.edu>
2025-06-19 10:00:02 +0530

    updated ticker

--- src/components/FloatingTicker.js
+++ src/components/FloatingTicker.js
@@ -39,12 +39,20 @@ const FloatingTicker = () => {
   }, []);
 
   return (
-    <div
+    <>
+      <div
       id="tradingview-widget-container"
        className="fixed top-0 left-0 w-full h-[40px] z-50 shadow-md bg-white dark:bg-black"
     >
       {/* TradingView widget script will be injected here */}
     </div>
+      <div
+        id="tradingview-widget-container"
+        className="fixed top-0 left-0 w-full h-[40px] z-50 shadow-md bg-white dark:bg-black"
+      >
+        {/* TradingView widget script will be injected here */}
+      </div>    
+    </>
   );
 };
 
