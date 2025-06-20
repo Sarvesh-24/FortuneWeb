a6cdde6a6af05f998c4ac33768e4a17c7d3e6a77
sarvesh <sarvesh.w@somaiya.edu>
2025-06-20 10:05:10 +0530

    updated ticker

--- src/App.js
+++ src/App.js
@@ -118,7 +118,7 @@ const App = () => {
               <Program />
               <Services />
               <Book />
-              {/* <Event /> */}
+              <Event />
               {/* <Feedback /> */}
               <Gallery />
               <Team />
